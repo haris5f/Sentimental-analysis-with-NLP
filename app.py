f45688e6e1ec353cd6b3ed07ce174b99ede8afc3
Muhammad Haris <83180251+haris5f@users.noreply.github.com>
2022-06-05 08:39:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, request, jsonify, render_template
 from tensorflow import keras
 import pickle
 from keras.preprocessing.sequence import pad_sequences
-import librosa
+#import librosa
+from scipy.io import wavfile
+
 
 app = Flask(__name__)
 model = keras.models.load_model('model_sentiment')
@@ -41,7 +43,8 @@ def upload_file():
     if request.method == 'POST':
         f = request.files["audiofile"]
         if f:
-            x_t,sr_t = librosa.load(f,duration=3, offset=0, res_type='kaiser_fast')  # read audio file
+            #x_t,sr_t = librosa.load(f,duration=3, offset=0, res_type='kaiser_fast')  # read audio file
+            sr_t, x_t = wavfile.read('output.wav')
             x_t = extract_feature(x_t,sr_t)
             x_t =np.array(x_t)
             x_t = sc.transform(x_t.reshape(1, -1))
