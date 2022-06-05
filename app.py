379f7dfdab83c35c1eeb617c6c0aa7cbfe2c9305
Muhammad Haris <83180251+haris5f@users.noreply.github.com>
2022-06-05 06:41:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def upload_file():
     return render_template('index.html', prediction_audio='Sentiment: {}'.format(outp))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080)
